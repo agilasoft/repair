acb288d0d65bc68ea62bd7d701f0d7f56f9841a3
root <info@agilasoft.com>
2021-09-10 07:21:31 +0000

    new file:   repair/repair/custom/item.json 	new file:   repair/repair/custom/item_barcode.json 	modified:   repair/repair/doctype/repair_estimate/repair_estimate.js 	modified:   repair/repair/doctype/repair_estimate/repair_estimate.json 	modified:   repair/repair/doctype/repair_estimate/repair_estimate.py 	renamed:    repair/repair/doctype/repair_operation/repair_operation.py -> repair/repair/doctype/repair_estimate/repair_estimate.py.save 	modified:   repair/repair/doctype/repair_estimate_operation/repair_estimate_operation.json 	modified:   repair/repair/doctype/repair_estimate_sparepart/repair_estimate_sparepart.json 	deleted:    repair/repair/doctype/repair_operation/__init__.py 	deleted:    repair/repair/doctype/repair_operation/repair_operation.js 	deleted:    repair/repair/doctype/repair_operation/repair_operation.json 	deleted:    repair/repair/doctype/repair_operation/test_repair_operation.py 	modified:   repair/repair/doctype/repair_operation_sparepart/repair_operation_sparepart.json 	modified:   repair/repair/doctype/repair_order_operation/repair_order_operation.json 	modified:   repair/repair/doctype/repair_order_sparepart/repair_order_sparepart.json 	modified:   repair/repair/doctype/service_pack/service_pack.json 	modified:   repair/repair/doctype/service_pack_operation/service_pack_operation.json

--- repair/repair/doctype/repair_estimate/repair_estimate.js
+++ repair/repair/doctype/repair_estimate/repair_estimate.js
@@ -2,7 +2,47 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Repair Estimate', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__("Repair Order"), function() {
+                        cur_frm.call('create_repair_order', function(r){});
+                        cur_frm.refresh_fields(frm);
+                }, __("Create"));
+		frm.add_custom_button(__("Service Pack"), function() {
+                        service_pack_dialog(frm);
+                }, __("Add"));		
+	}
 });
+
+let service_pack_dialog = function(frm) {
+	let dialog = new frappe.ui.Dialog({
+		title: 'Add Service Pack',
+		width: 100,
+		fields: [
+			{fieldtype: 'Link', label: 'Service Pack', fieldname: 'service_pack',
+				options: 'Service Pack'
+			}
+		],
+		primary_action_label: __('Add'),
+		primary_action : function(){
+			let service_pack = dialog.get_value('service_pack');
+			frappe.call({
+				doc: frm.doc,
+				method: 'add_service_pack',
+				args:{
+					'service_pack': service_pack
+				},
+				callback: function(data) {
+					if (!data.exc) {
+						frm.reload_doc();
+					}
+				},
+				freeze: true,
+				freeze_message: __('Adding Service Pack Items')
+			});
+			frm.refresh_fields();
+			dialog.hide();
+		}
+	});
+
+	dialog.show();
+};
--- repair/repair/doctype/repair_estimate/repair_estimate.py
+++ repair/repair/doctype/repair_estimate/repair_estimate.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import utils
 from frappe.model.document import Document
 
 class RepairEstimate(Document):
@@ -11,7 +12,70 @@ class RepairEstimate(Document):
     
     @frappe.whitelist()
     def create_repair_order(self):
-        return 0
-    
-    def test_me(self):
-        return 0
+        doc = frappe.new_doc("Repair Order")
+        doc.customer = self.customer
+        doc.order_type = self.order_type
+        doc.company = self.company
+        doc.date = utils.now()
+        doc.repair_estimate = self.name
+        doc.appointment = self.appointment
+        doc.billto_customer = self.billto_customer
+        doc.billto_insurance = self.billto_insurance
+        doc.billto_insurance = self.billto_insurance
+        doc.customer_pl = self.customer_pl
+        doc.insurance_pl = self.insurance_pl
+        doc.warranty_pl = self.warranty_pl
+        doc.currency = self.currency
+        doc.conversion_rate = self.conversion_rate
+        doc.selling_price_list = self.selling_price_list
+        doc.price_list_currency = self.price_list_currency
+        doc.plc_conversion_rate = self.plc_conversion_rate
+        doc.operations = self.operations
+        doc.spareparts = self.spareparts
+        doc.tax_category = self.tax_category
+        doc.taxes_and_charges = self.taxes_and_charges
+        doc.taxes = self.taxes
+        doc.payment_terms_template = self.payment_terms_template
+        doc.payment_schedule = self.payment_schedule
+        doc.tc_name = self.tc_name
+        doc.terms = self.terms
+        doc.campaign = self.campaign
+        doc.source = self.source
+        doc.status = self.status
+        doc.supplier_quotation = self.supplier_quotation
+        doc.insert()
+        
+        frappe.msgprint(doc.name + " was successfully created.")
+
+    @frappe.whitelist()
+    def add_service_pack(self, service_pack):
+        sp = frappe.db.sql("""SELECT * FROM `tabService Pack Operation`
+            WHERE parent = %s
+            """, (service_pack), as_dict=1)
+        
+        for i in sp:
+            op = frappe.get_doc("Item", i.repair_operation)
+            self.append("operations",
+                {
+                    'repair_operation': op.name,
+                    'description': op.description,
+                    'item_name': op.item_name,
+                    'uom': i.uom,
+                    'qty': op.standard_qty
+                })
+            
+            spareparts = frappe.db.sql("""SELECT * FROM `tabRepair Operation Sparepart`
+                WHERE parent = %s""", (i.repair_operation), as_dict=1)
+
+            for p in spareparts:
+                self.append("spareparts",
+                    {
+                        'operation': i.repair_operation,
+                        'item_code': p.item,
+                        'item_name': p.item_name,
+                        'description': p.item_name,
+                        'stock_uom': p.uom,
+                        'uom': p.uom,
+                        'stock_qty': p.qty,
+                        'qty': p.qty
+                    })
